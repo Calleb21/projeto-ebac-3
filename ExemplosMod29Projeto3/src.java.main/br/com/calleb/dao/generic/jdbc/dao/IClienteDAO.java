93b4011c884547e02a08d2d42507eec733a14913
Calleb Camargo <113070078+Calleb21@users.noreply.github.com>
2023-09-04 14:03:46 -0300

    ebac-projeto-03

---
+++ ExemplosMod29Projeto3/src.java.main/br/com/calleb/dao/generic/jdbc/dao/IClienteDAO.java
@@ -0,0 +1,22 @@
+package br.com.calleb.dao.generic.jdbc;
+
+import br.com.calleb.domain.Cliente;
+
+import java.util.List;
+
+/**
+ * Description of IClienteDAO
+ * Created by calle on 30/08/2023.
+ */
+public interface IClienteDAO {
+
+    public Integer cadastrar(Cliente cliente) throws Exception;
+
+    public Integer atualizar(Cliente cliente) throws Exception;
+
+    Cliente consultar(String codigo) throws Exception;
+
+    List<Cliente> buscarTodos() throws Exception;
+
+    Integer excluir(Cliente clienteBD) throws Exception;
+}
---
+++ ExemplosMod29Projeto3/src.java.main/br/com/calleb/dao/generic/jdbc/dao/IProdutoDAO.java
@@ -0,0 +1,22 @@
+package br.com.calleb.dao.generic.jdbc.dao;
+
+import br.com.calleb.domain.Produto;
+
+import java.util.List;
+
+/**
+ * Description of IProdutoDAO
+ * Created by calle on 01/09/2023.
+ */
+public interface IProdutoDAO {
+    Integer cadastrar(Produto produto) throws Exception;
+
+    Integer atualizarProd(Produto produtoBD) throws Exception;
+
+    Produto consultar(String codigo) throws Exception;
+
+    List<Produto> buscarTodosProdutos() throws Exception;
+
+    Integer excluir(Produto produtoBD) throws Exception;
+
+}
